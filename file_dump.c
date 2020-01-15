bd3e10303187d8a1a354905416d64e9c0eb5be92
Masaki17 <km.17.hayride@gmail.com>
2020-01-15 22:32:03 +0900

    Add file_dump script by c

---
+++ file_dump.c
@@ -0,0 +1,40 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int main(void)
+{
+  int n;
+  unsigned long count = 0;
+  unsigned char buf[16];
+  FILE *fp;
+  char fname[64];
+
+  printf("ファイル名：");
+  scanf("%s", fname);
+
+  if((fp = fopen(fname, "rb")) == NULL)
+    printf("\aファイルをオープンできません。\n");
+
+  else{
+    printf("         +0 +1 +2 +3 +4 +5 +6 +7 +8 +9 +A +B +C +D +E +F\n");
+    printf("--------------------------------------------------------\n");
+    while((n = fread(buf, 1, 16, fp)) > 0){
+      int i;
+      printf("%08lX ", count);
+
+      for(i = 0; i < n; i++)
+          printf("%02X ", (unsigned)buf[i]);
+
+      if(n < 16)
+          for(i = n; i < 16; i++) printf(" ");
+      for(i = 0; i < n; i++)
+          putchar(isprint(buf[i]) ? buf[i] : '.');
+      putchar('\n');
+
+      count += 16;
+    }
+    fclose(fp);
+  }
+
+  return(0);
+}
